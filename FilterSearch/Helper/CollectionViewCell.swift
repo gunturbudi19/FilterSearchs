7a240fc4c3a3fc9bbe9c405e09abcd055d1fb578
Guntur Budi <guntur.budi19@gmail.com>
2018-11-02 21:57:49 +0700

    Initial Helper

---
+++ FilterSearch/Helper/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  FilterSearch
+//
+//  Created by Guntur Budi on 02/11/18.
+//  Copyright © 2018 Test. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
